0b676477b131c0decde212b2f61937b72db5a130
Dinesh <dineshr93@gmail.com>
2023-07-25 16:49:43 +0200

    Add arguments intake

--- pdfmerge.py
+++ pdfmerge.py
@@ -1,16 +1,30 @@
 # from py import PdfMerger
 #  pip install PyPDF2
+import argparse
+
+arg_parser = argparse.ArgumentParser( description = "Copy source_file as target_file." )
+arg_parser.add_argument( "name_merged_file" )
+arg_parser.add_argument( "first_file" )
+arg_parser.add_argument( "second_file" )
+arguments = arg_parser.parse_args()
+
+first_file = arguments.first_file
+second_file = arguments.second_file
+name_merged_file = arguments.name_merged_file
+print( "Copying [{}] to [{}]".format(source, target) )
 try:
     from PyPDF2 import PdfMerger
 except ImportError:
     from pyPdf import PdfMerger
 
-pdfs = ['pdf1.pdf', 'pdf2.pdf']
+pdfs = [first_file, second_file]
 
 merger = PdfMerger()
 
 for pdf in pdfs:
     merger.append(pdf)
 
-merger.write("merged.pdf")
+if '.pdf' not in name_merged_file:
+    name_merged_file = name_merged_file + '.pdf'
+merger.write(name_merged_file)
 merger.close()
